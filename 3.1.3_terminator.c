995176730961dbf25e6cd94f2cde665064eebf6f
gramiras87 <85349673+gramiras87@users.noreply.github.com>
2022-10-24 20:17:34 +0300

    Create 3.1.3_terminator.c

---
+++ 3.1.3_terminator.c
@@ -0,0 +1,13 @@
+/*
+Исправьте предложенную задачу так, чтобы она выводила следующий текст:
+
+"I'll be back!" 
+	(c)Terminator
+*/
+
+#include <stdio.h>
+
+int main(void){
+  printf("\"I'll be back!\"\n\t(c)Terminator");
+  return 0;
+}
