2a04485bde7426f26ae0ac7c6e536de605a7928a
gramiras87 <85349673+gramiras87@users.noreply.github.com>
2022-10-24 19:25:03 +0300

    Create 1.1_Neo.c

---
+++ 1.1_Neo.c
@@ -0,0 +1,38 @@
+/*
+Напишите программу, которая выводит на экран следующие строки:
+
+NICKNAME: Neo
+CITY: St-Petersburg
+AGE: 35
+HEIGHT: 180
+WEIGHT: 75
+*/
+
+#include <stdio.h>
+
+int main() {
+  printf("NICKNAME: Neo\nCITY: St-Petersburg\nAGE: 35\nHEIGHT: 180\nWEIGHT: 75\n");
+  return 0;
+}
+
+/*
+// Структуры
+
+#include <stdio.h>
+
+struct Person {
+	char nickname[50];
+	char city[50];
+	int age;
+	int height;
+	int weight;
+};
+
+int main(void) {
+
+	struct Person Neo = { "Neo", "St-Petersburg", 35, 180, 75 };
+ 	printf("NICKNAME: %s\nCITY: %s\nAGE: %d\nHEIGHT: %d\nWEIGHT: %d\n", Neo.nickname, Neo.city, Neo.age, Neo.height, Neo.weight);
+	
+	return 0;
+}
+*/
